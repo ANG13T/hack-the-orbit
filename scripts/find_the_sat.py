272d2be08f111c1e7a8a4318c2f90f9480e67db1
angelina-tsuboi <angelina.t1832@gmail.com>
2024-09-12 14:13:42 -0700

    feat: init files

---
+++ scripts/find_the_sat.py
@@ -0,0 +1,14 @@
+"""
+ --- SATELLITE HACKING SEQUENCE CTF ---
+
+          Created by G4LXY
+"""
+
+from rich.console import Console
+
+console = Console()
+
+art = """""""
+
+def init_intro():
+    return
