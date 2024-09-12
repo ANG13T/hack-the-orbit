9bbaa2a157e638663bd1483e837a165b037d4fe9
g4lxy <angelinatsuboi@proton.me>
2024-09-12 15:33:01 -0700

    feat: init phase 0

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# Hack the Orbit Interactive for the Online Udemy Course 
+# Hack the Orbit
+
+## Setting Up
+
+1. Download Python and Pip3
+
+```
+# Verify Python3 is installed
+python3 --version
+
+# Verify Pip3 is installed
+pip3 --version
+```
+
+2. Clone the Repository
+
+```
+git clone https://github.com/ANG13T/hack-the-orbit.git
+```
+
+3. Install the Required Libraries
+
+```
+pip3 install -r requirements.txt
+```
+
+4. Run the Program
+
+```
+python3 main.py
+```
+
+
+### Notes to Self
--- scripts/find_the_sat.py
+++ scripts/find_the_sat.py
@@ -5,10 +5,72 @@
 """
 
 from rich.console import Console
+import os
 
 console = Console()
 
-art = """""""
+OPTIONS = [
+    ["START CHALLENGE", "ABOUT", "EXIT"]
+]
+
+# DISPLAY FUNCTIONS
+
+def banner():
+    f = open('banner.txt', 'r')
+    file_contents = f.read()
+    console.print("\n [cyan]" + file_contents + "[/]")
+    f.close()
+
+def display_options(index):
+    row = OPTIONS[index]
+    for i in range(len(row)):
+        console.print(f"{' ' * 10}[cyan]{i + 1}. {row[i]}\n[/]")
 
 def init_intro():
-    return
+    banner()
+
+def retrieve_input(options_index):
+    user_input = console.input("ENTER YOUR CHOICE: ")
+    try:
+        user_input = int(user_input)
+        if user_input > 0 and user_input <= len(OPTIONS[options_index]):
+            return user_input
+        else:
+            console.print("[red]Invalid option! Please try again.[/]")
+            return retrieve_input(options_index)
+    except ValueError:
+        console.print("[red]Invalid option! Please try again.[/]")
+        return retrieve_input(options_index)
+
+# CHALLENGE FUNCTIONS
+
+def start_challenge():
+    pass
+
+def about():
+    file_dir = os.path.dirname(os.path.realpath('__file__'))
+    file_name = os.path.join(file_dir, './options/about.txt')
+    f = open(file_name, 'r')
+    file_contents = f.read()
+    console.print("\n[cyan]" + file_contents + "[/]")
+    f.close()
+
+def exit():
+    print("Exiting program...")
+    os._exit(0)
+
+
+# OPTION DICTIONARY
+
+switch = {
+    0: start_challenge,
+    1: about,
+    2: exit
+}
+
+if __name__ == "__main__":
+    init_intro()
+    display_options(0)
+    choice = retrieve_input(0)
+    func = switch.get(choice - 1, None)
+    func()
