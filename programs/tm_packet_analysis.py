ba69a74138b06060f6d0572858977289b5a03c3e
g4lxy <angelinatsuboi@proton.me>
2024-09-13 09:19:44 -0700

    feat: init TM packet analysis

--- programs/tm_packet_analysis.py
+++ programs/tm_packet_analysis.py
@@ -1,2 +1,9 @@
+from .load_answer import load_answers
+from rich.console import Console
+from .story_formatter import process_story
+import os
+
+console = Console()
+
 def tm_packet_analysis():
     pass
